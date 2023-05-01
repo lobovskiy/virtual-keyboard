dd5e91e1d14c3edfc1796d5907ba8c3a3939993c
Artem Lobovskii <lobovskiy@yandex.ru>
2023-05-01 11:35:52 +0300

    feat: add permanent focus to the textarea

--- src/view/renderInputField.js
+++ src/view/renderInputField.js
@@ -1,10 +1,26 @@
 function renderInputField(container) {
   const inputDiv = document.createElement('div');
   inputDiv.classList.add('input-field');
-  inputDiv.innerHTML = `
-    <textarea id="input-field"></textarea>
-  `;
 
+  const textarea = document.createElement('textarea');
+  textarea.id = 'input-field';
+  textarea.setAttribute('autofocus', '');
+
+  function preventFocusLoss() {
+    const t = this;
+
+    function setFocus() {
+      t.focus();
+    }
+    setFocus();
+
+    // Some browsers won't set focus it until after the blur has completed
+    setTimeout(setFocus, 100);
+  }
+
+  textarea.addEventListener('blur', preventFocusLoss);
+
+  inputDiv.append(textarea);
   container.append(inputDiv);
 }
 
